c561ae341bb63af2a777968356cf7897ceb434fd
Tanawat Hongthai <ztrixack.th@gmail.com>
2023-06-20 16:17:30 +0700

    🐛 fix(main.ts): add version control to clear cache and update app version in local storage

--- apps/svelte-app/src/main.ts
+++ apps/svelte-app/src/main.ts
@@ -4,8 +4,29 @@ import '~/assets/css/global.css';
 
 import App from './App.svelte';
 
+// Get the saved version from local storage
+const savedVersion = localStorage.getItem('appVersion');
+
+// Define the current version of your app
+const currentVersion = '1.3.21';
+
+// Check if the saved version matches the current version
+if (savedVersion !== currentVersion) {
+  // Clear the cache
+  if ('caches' in window) {
+    caches.keys().then(function (cacheNames) {
+      cacheNames.forEach(function (cacheName) {
+        caches.delete(cacheName);
+      });
+    });
+  }
+
+  // Save the current version in local storage
+  localStorage.setItem('appVersion', currentVersion);
+}
+
 const app = new App({
   target: document.getElementById('app'),
-})
+});
 
-export default app
+export default app;
